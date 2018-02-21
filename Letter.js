ccc688517e9aac01148196655a4c60e19df4a3b0
KDarch928 <Kayle.Drucker@gmail.com>
2018-02-21 00:13:22 -0600

    worked on creating the Word constructor

--- Letter.js
+++ Letter.js
@@ -1,72 +1,99 @@
-var Letter = function (word) {
-    this.word = word;
-    this.displayedWord; //this will be used to display the letter or underscores
+// var Letter = function (word) {
+//     this.word = word;
+//     this.displayedWord; //this will be used to display the letter or underscores
+//     this.checker = false;
+//     this.guessLetter = "";
+//     this.letterCheker = "";
+//     this.underlyingChar = function () {
+//         //TODO
+//         //if true then check the word and return with the underscores with the letter guessed
+//         this.displayedWord = "";
+//         for(var i = 0; i < word.length; i++){
+//             if (word[i] === " " ){
+//                 this.displayedWord += " ";
+//             } else if (word[i] === this.guessLetter){
+//                 this.displayedWord += this.guessLetter;
+//             } else if (word[i] === this.letterCheker[i]){
+//                 this.displayedWord += this.letterCheker[i];
+//             } else {
+//                 this.displayedWord += "_";
+//             }
+//         }
+//         this.letterCheker = this.displayedWord;
+//         this.checker = false;
+//         console.log(this.displayedWord);
+//     }
+//     this.charChecker = function (guessedLetter) {
+//         //TODO
+//
+//         //check the guessed letter against the stored word
+//         for(var i = 0; i < word.length; i++){
+//             //if the letter matches a letter in the word, then set the checker to true and assign the letter to guessLetter
+//             if (word[i] === guessedLetter) {
+//                 this.checker = true;
+//                 this.guessLetter = guessedLetter;
+//             }
+//         }
+//
+//         //if it true, then set the checker boolean to true and run the underlyingChar function
+//         //otherwise display the current displayedWord var.
+//         if(this.checker) {
+//             this.underlyingChar();
+//         } else {
+//             console.log("this is false");
+//             console.log(this.displayedWord);
+//         }
+//
+//
+//     }
+// }
+
+var Letter = function (letter) {
+    this.letter = letter; //hold the letter
     this.checker = false;
-    this.guessLetter = "";
-    this.letterCheker = "";
-    this.underlyingChar = function () {
-        //TODO
-        //if true then check the word and return with the underscores with the letter guessed
-        this.displayedWord = "";
-        for(var i = 0; i < word.length; i++){
-            if (word[i] === " " ){
-                this.displayedWord += " ";
-            } else if (word[i] === this.guessLetter){
-                this.displayedWord += this.guessLetter;
-            } else if (word[i] === this.letterCheker[i]){
-                this.displayedWord += this.letterCheker[i];
-            } else {
-                this.displayedWord += "_";
-            }
+    this.underlyingChar = function (){
+        //if true, then set the checker to false and return the letter that has be guessed
+        // Otherwise return an underscore) if the letter has not been guessed
+        if(this.checker) {
+            this.checker = false;
+            return this.letter + " ";
+            console.log(this.letter);
+        } else if (this.letter === " "){
+            return "  ";
+            console.log(" ");
+        } else {
+            return "_ ";
+            console.log("_ ");
         }
-        this.letterCheker = this.displayedWord;
-        this.checker = false;
-        console.log(this.displayedWord);
     }
     this.charChecker = function (guessedLetter) {
-        //TODO
-
-        //check the guessed letter against the stored word
-        for(var i = 0; i < word.length; i++){
-            //if the letter matches a letter in the word, then set the checker to true and assign the letter to guessLetter
-            if (word[i] === guessedLetter) {
-                this.checker = true;
-                this.guessLetter = guessedLetter;
-            }
+        //if the guessed letter is guessed, then set the checker to true
+        if(guessedLetter === this.letter) {
+            this.checker = true;
         }
-
-        //if it true, then set the checker boolean to true and run the underlyingChar function
-        //otherwise display the current displayedWord var. 
-        if(this.checker) {
-            this.underlyingChar();
-        } else {
-            console.log("this is false");
-            console.log(this.displayedWord);
-        }
-
-       
+        //run the underlaying character
+        // this.underlyingChar();
     }
+
 }
+
 
 //exporting the Letter constructor
 module.exports =  Letter;
 
-var w = new Letter("jump");
+// var w = new Letter("j");
 
 
-
-w.underlyingChar();
-
-w.charChecker("M");
-
-
-w.charChecker("e");
-
-
-
-w.charChecker("j");
-
-w.charChecker("l");
-
-w.charChecker("m");
+// w.charChecker("M");
+//
+//
+// w.charChecker("e");
+//
+//
+//
+// w.charChecker("j");
+//
+// w.charChecker("l");
+//
+// w.charChecker("m");
 
--- Word.js
+++ Word.js
@@ -1,1 +1,65 @@
-var letter = require("/Letter");
+var Letter = require("./Letter");
+
+var Word = function () {
+    this.letterArr = [];
+    this.str = "";
+    this.displayStr = "";
+    this.addWord = function (word) {
+        for (var i = 0; i < word.length; i++){
+            this.letterArr.push(new Letter(word[i]));
+        }
+    }
+    this.returnString = function () {
+        //TODO
+        this.str = "";
+        for (var i = 0; i < this.letterArr.length; i++){
+            var l = this.letterArr[i].underlyingChar();
+            this.str += l;
+        }
+
+        if (this.displayStr === ""){
+            this.displayStr = this.str;
+        }
+
+        for (var j = 0; j < this.displayStr.length; j++){
+            if (this.str[j] !== "_" && this.str[j] !== " "){
+                this.displayStr = this.setCharAt(this.displayStr, j, this.str[j]);
+            }
+        }
+        console.log(this.displayStr);
+    }
+    this.setCharAt = function (str,index,chr) {
+        if(index > str.length-1) return str;
+        return str.substr(0,index) + chr + str.substr(index+1);
+    }
+    this.guess = function (guessedLetter) {
+
+        for (var item in this.letterArr) {
+            this.letterArr[item].charChecker(guessedLetter);
+        }
+    }
+
+};
+
+module.exports = Word;
+
+var newWord = new Word()
+
+newWord.addWord("Man of Steel");
+newWord.returnString();
+
+newWord.guess("j");
+newWord.returnString();
+
+newWord.guess("M");
+newWord.returnString();
+
+newWord.guess("f");
+newWord.returnString();
+
+newWord.guess("e");
+newWord.returnString();
+
+newWord.guess("a");
+newWord.returnString();
+
